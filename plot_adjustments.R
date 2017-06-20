0df8620a6c8e9ae6735451917cd6bab32f5a1f9d
adamtclark <adam.tclark@gmail.com>
2017-06-20 17:47:53 -0500

    Updated to match older test (from 4380d7e3e360e02118d3b3d1cbfdb22fbd190739 plot_adjustments.R) - now tests whether changing poa comp. matters

--- plot_adjustments.R
+++ plot_adjustments.R
@@ -233,13 +233,14 @@ subs<-which(agmondatall$monoculture%in%c("Poa pratensis", "Achillea millefolium(
 agmondatall<-agmondatall[subs,]
 
 plot(c(5.5, 8.5), c(15, 170), type="n", xlab="", ylab="", axes=F, log="y")
-put.fig.letter("G.", location = "topleft", offset=c(0.05, -0.05), cex=1.5)
-
 axis(2); axis(1, 6:8, c("June", "July", "August")); box()
+
 subslst<-as.character(sort(unique(agmondatall$monoculture)))
 adj<-seq(-0.1, 0.1, length=length(subslst))
+
 for(i in 1:length(subslst)) {
   subs<-which(agmondatall$monoculture==subslst[i])
+  
   mupp<-tapply(agmondatall$abvbiomass[subs], agmondatall$month[subs], function(x) exp(mean(log(x[is.finite(log(x))]))))
   sdpp<-tapply(agmondatall$abvbiomass[subs], agmondatall$month[subs], function(x) (sd(log(x[is.finite(log(x))]))/sqrt(length(x[is.finite(log(x))]))))
   
@@ -251,26 +252,19 @@ for(i in 1:length(subslst)) {
   points(6:8+adj[i], mupp, pch=c(9,12,14)[i], cex=1.5, lwd=1.5)
 }
 
-
 mtext("Month", side = 1, line=2.5)
 mtext(expression(paste(italic("B"), "*"[mono],", g m"^-2, sep="")), side = 2, line=2.5)
 
 #plot fit
 datout_old<-datoutlst[[2]] #no within-species variation
 includesp<-c("Poapr", "Achmi", "Liaas")
+
 lims<-c(0.1, 150)
 ul<-10^(log10(lims[1])+(log10(lims[2])-log10(lims[1]))*0.6)
 bl1<-10^((log10(lims[2])-log10(lims[1]))*0.125+log10(lims[1]))
 bl2<-10^((log10(lims[2])-log10(lims[1]))*0.02+log10(lims[1]))
 
 #get p-values
-datout_altered_early<-datoutlst[[2]] #With intrasp. variation
-datout_old<-datoutlst[[4]] #Without intrasp. variation
-includesp<-c("Achmi", "Poapr")
-
-datout_altered_early$obs[datout_altered_early$obs<replacelower]<-replacelower
-datout_altered_early$est[datout_altered_early$est<replacelower]<-replacelower
-
 datout_altered_early$obs[datout_altered_early$obs<replacelower | !is.finite(datout_altered_early$obs)]<-replacelower
 datout_altered_early$est[datout_altered_early$est<replacelower | !is.finite(datout_altered_early$est)]<-replacelower
 
@@ -283,7 +277,8 @@ datout_old$est[datout_old$est<replacelower | !is.finite(datout_old$est)]<-replac
 datout_old$res<-abs(log10(datout_old$obs)-log10(datout_old$est))
 tmp2<-with(datout_old[datout_old$sp%in%includesp & datout_old$plantedsr>1,], aggregate(cbind(res=res), list(plt=plt), mean))
 
-suppressWarnings(pv<-wilcox.test(tmp1$res, tmp2$res, paired=T)$p.value)
+pv<-wilcox.test(tmp1$res, tmp2$res, paired=T)$p.value
+
 ag_new<-with(datout_altered_early[datout_altered_early$sp%in%includesp & datout_altered_early$plantedsr>1,],
              aggregate(cbind(obs, est),
                        list(sp=sp, plantedsr=plantedsr),
@@ -299,7 +294,6 @@ plot(ag_old$est[,1], ag_old$obs[,1], pch=c(18,15,15)[as.numeric(as.factor(ag_old
      xlab="",
      ylab="",
      type="n")
-put.fig.letter("H.", location = "topleft", offset=c(0.05, -0.05), cex=1.5)
 
 segments(ag_old$est[,1], exp(log(ag_old$obs[,1])+ag_old$obs[,2]/sqrt(ag_old$obs[,3])), ag_old$est[,1], exp(log(ag_old$obs[,1])-ag_old$obs[,2]/sqrt(ag_old$obs[,3])), lend=2, lwd=1.51)
 segments(exp(log(ag_old$est[,1])+ag_old$est[,2]/sqrt(ag_old$est[,3])), ag_old$obs[,1], exp(log(ag_old$est[,1])-ag_old$est[,2]/sqrt(ag_old$est[,3])), ag_old$obs[,1], lend=2, lwd=1.51)
@@ -320,7 +314,6 @@ plot(ag_new$est[,1], ag_new$obs[,1], pch=c(18,15,15)[as.numeric(as.factor(ag_new
      xlab="",
      ylab="",
      type="n")
-put.fig.letter("I.", location = "topleft", offset=c(0.05, -0.05), cex=1.5)
 
 segments(ag_new$est[,1], exp(log(ag_new$obs[,1])+ag_new$obs[,2]/sqrt(ag_new$obs[,3])), ag_new$est[,1], exp(log(ag_new$obs[,1])-ag_new$obs[,2]/sqrt(ag_new$obs[,3])), lend=2, lwd=1.51)
 segments(exp(log(ag_new$est[,1])+ag_new$est[,2]/sqrt(ag_new$est[,3])), ag_new$obs[,1], exp(log(ag_new$est[,1])-ag_new$est[,2]/sqrt(ag_new$est[,3])), ag_new$obs[,1], lend=2, lwd=1.51)
@@ -335,7 +328,6 @@ points(ag_new$est[,1], ag_new$obs[,1], pch=c(9,12,14)[as.numeric(as.factor(ag_ne
        cex=1.4)
 text(ul, bl1, paste("MAE =", round(10^mean(tmp1$res)-1, 2)), adj=c(0,0), cex=1.2)
 text(ul, bl2, paste("p <", ceiling(pv*1000)/1000), adj=c(0,0), cex=1.2)
-
 
 
 
